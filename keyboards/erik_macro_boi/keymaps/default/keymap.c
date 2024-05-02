9c8de37fb3efa50bb9e1a705543b0b84468b85b0
dennis <none>
2024-05-02 14:01:48 -0500

    Funky keys

--- keyboards/erik_macro_boi/keymaps/default/keymap.c
+++ keyboards/erik_macro_boi/keymaps/default/keymap.c
@@ -3,9 +3,27 @@
 
 #include QMK_KEYBOARD_H
 
+enum custom_keycodes {
+    ERIK_MACRO = SAFE_RANGE
+};
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT_ortho_4x4(
-        KC_A,   KC_B,   KC_C,  KC_C,
-        KC_A,   KC_B,   KC_C,  KC_C
+            ERIK_MACRO
     )
 };
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    switch(keycode) {
+        case ERIK_MACRO:
+            if (record->event.pressed) {
+                SEND_STRING("Happy birthday!!");
+            }
+            else {
+                SEND_STRING(" This keyboard is broken");
+            }
+            break;
+    }
+
+    return true;
+}
